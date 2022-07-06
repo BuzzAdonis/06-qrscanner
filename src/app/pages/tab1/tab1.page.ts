aff0b29f0ea1f119769ffabe3515a6c4f8c845b4
Adonis A. Severino <adonisalexander@hotmail.com>
2022-07-05 23:20:49 -0400

    install cordova

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  swiopeOptions = {
+    allowSlidePrev:false,
+    allowSlideNext:false
+  }
+  ionViewDidEnter(){
+    console.log("ionViewDidEnter es cuando la pagina carga");
+  }
+  ionViewDidLeave(){
+    console.log("ionViewDidLeave es cuando la pagina sale de vista");
+  }
+  ionViewWillEnter(){
+    console.log("ionViewWillEnter va a cargar");
+    this.scan();
+  }
+  ionViewWillLeave(){
+    console.log("ionViewWillLeave ya cargo");
+  }
+  constructor(private barcodeScanner:BarcodeScanner) {}
+  scan(){
+    
+this.barcodeScanner.scan().then(barcodeData => {
+  console.log('Barcode data', barcodeData);
+ }).catch(err => {
+     console.log('Error', err);
+ });
+  }
 }
